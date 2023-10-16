ff20b386a3f3cdacfc27406fd19898bcba1b254c
SkySwell <duccoma@gmail.com>
2023-10-16 03:23:41 +0100

    change dummyMain

--- src/dummyMain.cpp
+++ src/dummyMain.cpp
@@ -5,25 +5,30 @@
 int main() {
     const std::string slackApiUrl =  "https://slack.com/api/chat.postMessage";
     const std::string slackApiToken = "input-your-slack-api-token";
-    const std::string xmlPath = "input-haarcascade-xml-file-path";
-
+    //const std::string xmlPath = "/usr/local/share/opencv4/haarcascades/haarcascade_frontalface_default.xml";
+    const std::string jsonPath = "../text/message.json";
+    int imgNum = 0;
     SlackMessenger messenger(slackApiUrl, slackApiToken);
     UltrasonicSensor sensor(4,5);
-    FaceDetect detector(xmlPath);
+    FaceDetect detector;
 
-    std::string payload = messenger.readJsonFilename("message.json");
+    std::string payload = messenger.readJsonFilename(jsonPath);
 
     while(1) {
         float dis = sensor.measureDistance();
         std::cout << dis << std::endl;
 
         if(dis <= 30) {
-            //std::cout << detector.detect(detector.takePic()) << std::endl;
-            if(detector.detect(detector.takePic()) == 1) {
+            imgNum++;
+            std::string imgPath = detector.takePic(imgNum);
+            if(detector.detect(imgPath) == 1) {
                 messenger.sendMessageToSlack(payload);
+            }else{
+                detector.deletePicture(imgPath);
+                imgNum--;
             }
         }
-        delay(3000);
+        delay(300);
     }
     return 0;
 }
